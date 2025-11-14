9ded2e78d5d3a88e9595a4c418cfe37bbf2532a5
Gabriel Elias <gabrielelias8@hotmail.com>
2025-11-14 09:28:40 -0300

    Hide Streamlit toolbar and profile container in app layout

--- app.py
+++ app.py
@@ -33,6 +33,17 @@ st.set_page_config(
     page_title="JC Contabilidade - Postos Buffon", layout="wide"
 )
 
+st.markdown(
+    """
+    <style>
+    .stAppToolbar, [class*="_profileContainer"], [class*="_container"][class*="_viewerBadge"] {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 st.title("🧾 JC Contabilidade - Postos Buffon")
 
 with st.container(border=True):
